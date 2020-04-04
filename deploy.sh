a1224d8f56f230d3fbef968ed1d9fbd9fe488992
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-04-04 20:35:43 -0300

    Altera run.sh e deploy.sh para incluir caso-full

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,30 @@ SCRIPT_PATH=$(dirname $(readlink -f $0))
 source $SCRIPT_PATH/base.sh
 DATASET="covid19"
 
+function upload_table_file() {
+	table=$1
+
+	log "[$table] Copying data to static file server"
+	s3cmd put data/output/${table}.csv.gz s3://dataset/$DATASET/${table}.csv.gz
+}
+
+function update_table() {
+	table=$1
+
+	# TODO: change collect-date on update command
+	log "[$table] Executing update command"
+	ssh $BRASILIO_SSH_USER@$BRASILIO_SSH_SERVER "$BRASILIO_UPDATE_COMMAND $DATASET $table"
+}
+
 log "Cleaning data path and collecting data"
 ./run.sh
 
 source $SCRIPT_PATH/.env
 for table in boletim caso; do
-	log "[$table] Copying data to static file server"
-	s3cmd put data/output/${table}.csv.gz s3://dataset/$DATASET/${table}.csv.gz
-
-	log "[$table] Executing update command"
-	ssh $BRASILIO_SSH_USER@$BRASILIO_SSH_SERVER "$BRASILIO_UPDATE_COMMAND $DATASET $table"
+	upload_table_file $table
+	update_table $table
 done
-# TODO: change collect-date on update command
-# TODO: generate status page for this dataset
+upload_table_file "caso-full"
 
 log "Generating file list page"
 python create_html.py dataset $DATASET $(date +"%Y-%m-%d") $SCRIPT_PATH/data/output/
--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ rm -rf $OUTPUT_PATH/*.csv.gz $ERROR_PATH
 mkdir -p "$OUTPUT_PATH" "$LOG_PATH" "$ERROR_PATH"
 caso_filename="$OUTPUT_PATH/caso.csv.gz"
 boletim_filename="$OUTPUT_PATH/boletim.csv.gz"
-
+full_filename="$OUTPUT_PATH/caso-full.csv.gz"
 time scrapy runspider consolida.py \
 	--loglevel="INFO" \
 	--logfile="$LOG_PATH/consolida.log" \
@@ -18,3 +18,4 @@ if [ $(ls $ERROR_PATH/errors-*.csv 2>/dev/null | wc -l) -gt 0 ]; then
 	# Some error happened
 	exit 255
 fi
+time python full.py "$caso_filename" "$full_filename"
